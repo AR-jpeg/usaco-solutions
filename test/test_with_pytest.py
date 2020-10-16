03b4179c99ce59cf2ef99d462de9cdb3075e7ad1
Aditya Rao Udupi <araoudupi@gmail.com>
2020-10-16 13:01:25 -0500

    Trying to update testcases

---
+++ test/test_with_pytest.py
@@ -0,0 +1,16 @@
+"""Test all the modules."""
+import os
+
+
+def test_dualpal():
+    with open('dualpal.in', 'w') as f:
+        f.write('3 25\n')
+
+    os.system('cp ../dualpal/dualpal.py')
+
+    with open('dualpal.out') as f:
+        assert(f.read() == """26\n27\n28\n""")
+
+    os.remove('dualpal.py')
+    os.remove('dualpal.in')
+    os.remove('dualpal.out')
